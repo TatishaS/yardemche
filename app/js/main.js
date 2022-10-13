48da30519e213bfe231ebb1e9abad3d22e2d9748
TatishaS <snow_white_girl@bk.ru>
2022-10-13 18:57:17 +0300

    Account: fixes in edit form (html+css)

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,69 @@
 'use strict';
 
+// Разрешаем скролл заднего фона при открытом модальном окне
+function enableScroll() {
+  const body = document.body;
+  let pagePosition = parseInt(body.dataset.position, 10);
+  body.style.top = 'auto';
+  body.classList.remove('disable-scroll');
+
+  window.scroll({ top: pagePosition, left: 0 });
+  //body.removeAttribute('data-position');
+}
+// Запрещаем скролл заднего фона при открытом модальном окне
+function disableScroll() {
+  const body = document.body;
+  //body.removeAttribute('data-position');
+  let pagePosition = window.scrollY;
+  body.classList.add('disable-scroll');
+  body.dataset.position = pagePosition;
+  body.style.top = -pagePosition + 'px';
+}
+
+/* Burger menu */
+function burgerMenu() {
+  const burger = document.querySelector('.menu__burger');
+  const menuList = document.querySelector('.menu__list');
+  const userNav = document.querySelector('.user-nav');
+  const overlay = document.querySelector('.overlay');
+  //const body = document.querySelector('body');
+  const menuLinks = document.querySelectorAll('.menu__list-link');
+
+  function hideBurgerMenu() {
+    menuList.classList.remove('active');
+    userNav.classList.remove('active');
+    burger.classList.remove('active-burger');
+    overlay.classList.remove('overlay--show');
+  }
+
+  burger.addEventListener('click', () => {
+    if (!menuList.classList.contains('active')) {
+      menuList.classList.add('active');
+      userNav.classList.add('active');
+      burger.classList.add('active-burger');
+
+      overlay.classList.add('overlay--show');
+    } else {
+      hideBurgerMenu();
+    }
+  });
+  /* Cкрываем мобильное меню по нажатию на ссылку в меню */
+  menuLinks.forEach(link => {
+    link.addEventListener('click', hideBurgerMenu);
+  });
+
+  // Брейкпойнт, на котором появляется бургер-меню
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 640) {
+      menuList.classList.remove('active');
+      userNav.classList.remove('active');
+      burger.classList.remove('active-burger');
+    }
+  });
+
+  overlay.addEventListener('click', hideBurgerMenu);
+}
+
 let swiper = new Swiper('.swiper-slider', {
   // Optional parameters
   slidesPerView: 1,
@@ -53,7 +117,7 @@ function bindModal(trigger, modal, close) {
 
   const hideModal = elem => {
     //console.log('Навешан обработчик');
-    //enableScroll();
+    enableScroll();
     modal.style.display = 'none';
     overlay.classList.remove('overlay--show');
     elem.removeEventListener('click', hideModal);
@@ -64,7 +128,7 @@ function bindModal(trigger, modal, close) {
       e.preventDefault();
       modal.style.display = 'block';
       overlay.classList.add('overlay--show');
-      //disableScroll();
+      disableScroll();
 
       /* Обработка загрузки превью изображения */
       handleImagePreview();
@@ -81,5 +145,7 @@ function bindModal(trigger, modal, close) {
     });
   });
 }
+
+burgerMenu();
 
 bindModal('.edit__btn', '.modal__edit', '.edit-form__close');
